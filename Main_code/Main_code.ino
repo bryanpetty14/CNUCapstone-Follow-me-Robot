30b50539cff5ba35b42de19f1077073d4b2ced93
bryanpetty14 <31447192+bryanpetty14@users.noreply.github.com>
2018-03-19 09:17:13 -0400

    Add files via upload
    Newest version as of 3/19/2018

--- Main_code/Main_code.ino
+++ Main_code/Main_code.ino
@@ -1,7 +1,15 @@
 
+#include <Wire.h>
+//#include <Adafruit_MotorShield.h>
 #include <AFMotor.h>
 #include <Servo.h>
 //CREATING MOTOR OBJECTS//
+//Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+/*Adafruit_DCMotor *motorone = AFMS.getMotor(1);
+  Adafruit_DCMotor *motortwo = AFMS.getMotor(2);
+  Adafruit_DCMotor *motorthree = AFMS.getMotor(3);
+  Adafruit_DCMotor *motorfour = AFMS.getMotor(4);
+*/
 AF_DCMotor motorone(1, MOTOR12_64KHZ);
 AF_DCMotor motortwo(2, MOTOR12_64KHZ);
 AF_DCMotor motorthree(3, MOTOR12_64KHZ);
@@ -9,13 +17,14 @@ AF_DCMotor motorfour(4, MOTOR12_64KHZ);
 Servo servo;
 
 //GLOBAL VARIABLES//
-int minValue = 100;
+int minValue = 200;
 long distance = 0;
-const int trigPin = A1;
-const int echoPin = A0;
+const int trigPin = A0;
+const int echoPin = A1;
 double aveDeg[3] = { -1, -1, -1};
 
 void setup() {
+  //AFMS.begin();
   motorone.setSpeed(0);
   motortwo.setSpeed(0);
   motorthree.setSpeed(0);
@@ -54,21 +63,25 @@ void sonar() {
 
 void changeSpeedSpecial(int degree) {
   if (degree == 0.00) {
-    motorone.setSpeed(50);
-    motortwo.setSpeed(50);
-    motorthree.setSpeed(50);
-    motorfour.setSpeed(50);
-    motorone.run(BACKWARD);
-    motortwo.run(BACKWARD);
+    motorone.setSpeed(85);
+    motortwo.setSpeed(85);
+    motorthree.setSpeed(85);
+    motorfour.setSpeed(85);
+    motorone.run(RELEASE);
+    motortwo.run(RELEASE);
     motorthree.run(FORWARD);
     motorfour.run(FORWARD);
+    motorone.run(BACKWARD);
+    motortwo.run(BACKWARD);
     Serial.println("Left");
   }
   else if (degree == 180.00) {
-    motorone.setSpeed(50);
-    motortwo.setSpeed(50);
-    motorthree.setSpeed(50);
-    motorfour.setSpeed(50);
+    motorone.setSpeed(85);
+    motortwo.setSpeed(85);
+    motorthree.setSpeed(85);
+    motorfour.setSpeed(85);
+    motorthree.run(RELEASE);
+    motorfour.run(RELEASE);
     motorone.run(FORWARD);
     motortwo.run(FORWARD);
     motorthree.run(BACKWARD);
@@ -79,8 +92,8 @@ void changeSpeedSpecial(int degree) {
   else {
     motorone.setSpeed(200);
     motortwo.setSpeed(200);
-    motorthree.setSpeed(200);
-    motorfour.setSpeed(200);
+    motorthree.setSpeed(200+10);
+    motorfour.setSpeed(200+10);
     motorone.run(FORWARD);
     motortwo.run(FORWARD);
     motorthree.run(FORWARD);
@@ -121,10 +134,10 @@ void onwards() {
   int minValue = 100;
   double degree, weightedAve = 0;
   for (int j; j < 180; j++) {
-    west[j] = analogRead(3);
-    south[j] = analogRead(2);
-    east[j] = analogRead(5);
-    north[j] = analogRead(4);
+    west[j] = analogRead(4);
+    south[j] = analogRead(5);
+    east[j] = analogRead(2);
+    north[j] = analogRead(3);
     //delay(1);
     if (west[j] <= minValue) {
       sumWest++;
@@ -141,9 +154,9 @@ void onwards() {
   }
   degree = 90.0;//90 because ¯\_(ツ)_/¯
 
-  if ((sumWest == 0 && sumEast == 0 && sumNorth == 0 && sumSouth == 0 ) || (sumWest != 0 && sumEast != 0 && sumNorth != 0 && sumSouth != 0) ) { //device off
+  if ((sumWest <= 5 && sumEast <= 5 && sumNorth <= 5 && sumSouth <= 5 ) || (sumWest != 0 && sumEast != 0 && sumNorth != 0 && sumSouth != 0) ) { //device off
     //reset degree values
-      Serial.println("stop");
+    Serial.println("stop");
     degree = -1;
     //clear array because of no direction
     aveDeg[2] = -1;
@@ -155,7 +168,7 @@ void onwards() {
   }
 
   else if (sumNorth > sumSouth && sumNorth > 20) {//detected something more north than south
-    if (sumNorth > 170 && sumEast < 20 && sumWest < 20) { //north much greater
+    if (sumNorth > 100 && sumEast <= 1 && sumWest <= 1) { //north much greater
       //just go north
       degree = 90;
       //clear array because of direction change
@@ -165,18 +178,18 @@ void onwards() {
     }
     else if (sumEast > sumWest) {//check if east is greater than west
 
-      /*else if (aveDeg[2] == 180) { //if just came from west
+      if (aveDeg[2] == 180) { //if just came from west
         aveDeg[2] = -1;//make the last value not count
-        }*/
+      }
 
       degree = atan2(sumEast, sumNorth);//take the arctan of opposite over adjacent
       degree *= 180 / 3.1415; //convert to degrees
       degree = 90 - degree;//adjust to find the new angle
     }
     else { //east is not bigger, use west
-      /*if (aveDeg[2] == 0) { //if just came from east
+      if (aveDeg[2] == 0) { //if just came from east
         aveDeg[2] = -1;//make the last value not count
-        }*/
+      }
       degree = atan2(sumWest, sumNorth);//take arctan of opposite over adjacent on left side(if degree = 0 after,then north has value and east and west are 0)
       degree *= 180 / 3.1415; //convert (if 0, will still be 0)
       degree += 90;//adjust degree to new angle
@@ -187,97 +200,99 @@ void onwards() {
   else if (sumEast > sumWest) {//east has greater value than west
     //if(sonar check)
     //clear array because of direction change
-    aveDeg[2] = -1;
-    aveDeg[1] = -1;
-    aveDeg[0] = -1;
+    /*aveDeg[2] = -1;
+      aveDeg[1] = -1;
+      aveDeg[0] = -1;*/
     degree = 0;
   } else { //west has the greater value than east
     //if(sonar Check)
     //clear array because of direction change
-    if(sumWest > 20){
-    aveDeg[2] = -1;
-    aveDeg[1] = -1;
-    aveDeg[0] = -1;
-    degree = 180;
+    if (sumWest > 20) {
+      /*aveDeg[2] = -1;
+        aveDeg[1] = -1;
+        aveDeg[0] = -1;*/
+      degree = 180;
     }
-    else if(aveDeg[2] == 0){
-    aveDeg[2] = -1;
-    aveDeg[1] = -1;
-    aveDeg[0] = -1;
-    degree = 0;
+    else if (aveDeg[2] == 0) {
+      /*aveDeg[2] = -1;
+        aveDeg[1] = -1;
+        aveDeg[0] = -1;*/
+      degree = 0;
     }
-    else{
-    aveDeg[2] = -1;
-    aveDeg[1] = -1;
-    aveDeg[0] = -1;
-    degree = 180;
+    else {
+      /*aveDeg[2] = -1;
+        aveDeg[1] = -1;
+        aveDeg[0] = -1;*/
+      degree = 180;
     }
   }
   Serial.println(degree);
   //if (degree != 90.00 && degree != 0.00 && degree != 180.00) {
-    //shift values
-    aveDeg[0] = aveDeg[1];
-    aveDeg[1] = aveDeg[2];
-    aveDeg[2] = degree;
+  //shift values
+  aveDeg[0] = aveDeg[1];
+  aveDeg[1] = aveDeg[2];
+  aveDeg[2] = degree;
 
-    //declare variables for running average
-    sum = 0;
-    weightedAve = 0;
+  //declare variables for running average
+  sum = 0;
+  weightedAve = 0;
 
-    //find the valid values and add them to the average;
-    for (int j = 0; j < 3; j++) {
-      if (aveDeg[j] != -1) {
-        weightedAve += aveDeg[j];
-        sum++;
-      }
+  //find the valid values and add them to the average;
+  for (int j = 0; j < 3; j++) {
+    if (aveDeg[j] != -1) {
+      weightedAve += aveDeg[j];
+      sum++;
     }
+  }
 
-    //weight the last value by x2
-    if (aveDeg[2] != -1) {
-      weightedAve += aveDeg[2] * 2;
-      sum += 2;
-    }
+  //weight the last value by x2
+  if (aveDeg[2] != -1) {
+    weightedAve += aveDeg[2] * 2;
+    sum += 2;
+  }
 
-    //check to see if any values were valid and then average the values
-    if (sum != 0) {
-      weightedAve /= sum;
-    } else {
-      weightedAve = -1;
-    }
-    
-      Serial.print("North : ");
-      Serial.println(sumNorth);
-      Serial.print("West : ");
-      Serial.println(sumWest);
-      Serial.print("East : ");
-      Serial.println(sumEast); +
-      Serial.print("South : ");
-      Serial.println(sumSouth);
-      Serial.print("Degree(weighted average : ");
-      Serial.println(weightedAve);
-      Serial.println("");
-    
+  //check to see if any values were valid and then average the values
+  if (sum != 0) {
+    weightedAve /= sum;
+  }/* else{
+      stop();
+      return;
+    }*/
+  if (weightedAve != 90.00 && weightedAve != 0.00 && weightedAve != 180.00) {
+    Serial.print("North : ");
+    Serial.println(sumNorth);
+    Serial.print("West : ");
+    Serial.println(sumWest);
+    Serial.print("East : ");
+    Serial.println(sumEast); +
+    Serial.print("South : ");
+    Serial.println(sumSouth);
+    Serial.print("Degree(weighted average : ");
+    Serial.println(weightedAve);
+    Serial.println("");
+
 
     changeSpeed(weightedAve);
     motorone.run(FORWARD);
     motortwo.run(FORWARD);
     motorthree.run(FORWARD);
     motorfour.run(FORWARD);
-  /*} else {
-      Serial.print("North : ");
-      Serial.println(sumNorth);
-      Serial.print("West : ");
-      Serial.println(sumWest);
-      Serial.print("East : ");
-      Serial.println(sumEast); 
-      Serial.print("South : ");
-      Serial.println(sumSouth);
-      Serial.print("Degree(weighted average) : ");
-      Serial.println(degree);
-      Serial.println("");
-    changeSpeedSpecial(degree);
-  }*/
-  
+  }
+  else {
+    Serial.print("North : ");
+    Serial.println(sumNorth);
+    Serial.print("West : ");
+    Serial.println(sumWest);
+    Serial.print("East : ");
+    Serial.println(sumEast);
+    Serial.print("South : ");
+    Serial.println(sumSouth);
+    Serial.print("Degree(weighted average) : ");
+    Serial.println(degree);
+    Serial.println("");
+    changeSpeedSpecial(weightedAve);
+  }
+
   //reset sums
   sumWest = 0;
   sumEast = 0;
@@ -287,7 +302,7 @@ void onwards() {
 void loop() {
   sonar();
   Serial.println(distance);
-  if (true){//distance > 15) {
+  if (distance > 15) {
     onwards();
   } else {
     //turn the sonar sensor and check around the robot
